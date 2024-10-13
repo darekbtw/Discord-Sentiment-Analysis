f535033181f06694bbab9693ec38e1107891ab30
darekbtw <dareksnowak@gmail.com>
2024-10-13 10:52:01 -0500

    Added UI with Streamlit

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+import streamlit as st
+
 load_dotenv()
 nltk.download('vader_lexicon')
-
 sia = SentimentIntensityAnalyzer()
 
 def get_messages(channelId):
     headers = {
-        'authorization': os.environ.get('authorization')
-    }
+    'authorization': os.environ.get('authorization')
+}
 
     r = requests.get(f'https://discord.com/api/v9/channels/{channelId}/messages', headers=headers)
     data = json.loads(r.text)
@@ -36,20 +37,36 @@ def get_sentiment(compound):
 
 def analyze_messages(messages):
     total_score, total = 0, 0
+    results = []
 
     for message in messages:
         sentiment = sia.polarity_scores(message)
         compound = sentiment['compound']
         curr_sentiment = get_sentiment(compound)
-        total_score+=compound
-        total+=1
+        total_score += compound
+        total += 1
 
-        print(f"\"{message}\" is {curr_sentiment}.")
+        result = f"\"{message}\" is {curr_sentiment}."
+        results.append(result)
     
     avg_sentiment = float(total_score) / total
     overall_sentiment = get_sentiment(avg_sentiment)
-    print(f"\nSentiment: {overall_sentiment}\n")
+    results.append(f"\nOverall Sentiment: {overall_sentiment}\n")
+    return '\n'.join(results)
 
-input = input("Enter your Discord Channel ID: ")
-messages = get_messages(input)
-analyze_messages(messages)
+
+
+st.set_page_config(layout="wide", page_title="Discord Sentiment Analysis", page_icon="📰")
+st.title('Discord Sentiment Analysis')
+id_input = st.text_input("Enter a Discord Channel ID:", value="1073810997552349256")
+submit_button = st.button("Submit")
+
+if submit_button:
+    try:
+        messages = get_messages(id_input)
+        res = analyze_messages(messages)
+
+        st.code(res)
+    except Exception as e:
+        st.error(f"An Error Occurred: {e}")
+
